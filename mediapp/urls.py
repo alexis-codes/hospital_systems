aae168dda3b1772a24635679c70f88fe15512517
alexis-codes <gogoalexis49@gmail.com>
2025-02-25 16:35:29 +0300

    initial commit

---
+++ mediapp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from mediapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index,name='index'),
+    path('starter/', views.starter,name='starter-page'),
+    path('about/', views.about ,name='about'),
+    path('services/', views.services,name='services'),
+    path('doctors/', views.doctors,name='doctors'),
+    path('departments/', views.departments,name='departments'),
+
+]
---
+++ mediapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def starter(request):
+    return render(request, 'starter-page.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def departments(request):
+    return render(request, 'departments.html')
+
+
